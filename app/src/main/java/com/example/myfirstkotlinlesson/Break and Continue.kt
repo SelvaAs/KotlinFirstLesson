c9349547f608c93bd3c36a3c50a6dc81ac78444c
selvi <se>
2023-06-16 06:34:36 -0400

    break and continue

---
+++ app/src/main/java/com/example/myfirstkotlinlesson/Break and Continue.kt
@@ -0,0 +1,24 @@
+package com.example.myfirstkotlinlesson
+
+fun main(){
+
+    for(i in 1 until 20) {
+        print("$i ")
+        if (i / 2 == 5) {
+            break
+        }
+    }
+        print("\nDone with the loop")
+
+        for (i in 1 until 20) {
+
+            // 10/2=5
+            // 11/2=5.5 which is 5 in term of an int
+            if (i / 2 == 5) {
+              continue
+            }
+            print("$i ") //1 2 3 4 5 6 7 8 9 12 13 14 15 16 17 18 19
+
+
+        }
+    }
