c24add83ee7044b8eff7e088af746bf38e023ef3
SelvaAs <136520563+SelvaAs@users.noreply.github.com>
2023-06-26 04:09:04 -0400

    Update 13-Absract Classes.kt

--- app/src/main/java/com/example/myfirstkotlinlesson/13-Absract Classes.kt
+++ app/src/main/java/com/example/myfirstkotlinlesson/13-Absract Classes.kt
@@ -1,6 +1,6 @@
 package com.example.myfirstkotlinlesson
 
-//A class may be declared abstract, along with some or all of its members.
+//A class may be declared abstract, along with some or all of its members
 // An abstract member does not have an implementation in its class.
 // You don't need to annotate abstract classes or functions with open.
 
@@ -14,7 +14,7 @@ class Rectangle1 : Polygon() {
     }
 }
 
-//You can override a non-abstract open member with an abstract one.
+//You can override a non-abstract open member with an abstract one
 open class Polygon1 {
     open fun draw() {
         // some default polygon drawing method
