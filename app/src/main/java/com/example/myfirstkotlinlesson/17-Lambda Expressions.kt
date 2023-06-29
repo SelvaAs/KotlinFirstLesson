d44717464f805a70f763331b459203daea55e4a6
SelvaAs <136520563+SelvaAs@users.noreply.github.com>
2023-06-29 00:33:17 -0400

    Update 17-Lambda Expressions.kt

--- app/src/main/java/com/example/myfirstkotlinlesson/17-Lambda Expressions.kt
+++ app/src/main/java/com/example/myfirstkotlinlesson/17-Lambda Expressions.kt
@@ -5,7 +5,7 @@ fun main(){
     val sum:(Int, Int) -> Int = {a:Int, b:Int ->a+b}
     println(sum(10,5))
 
-    //even shorter
+    //even shorter.
     val sum1 = {a:Int, b:Int -> println(a+b)}
     sum1(10,5)
 }
