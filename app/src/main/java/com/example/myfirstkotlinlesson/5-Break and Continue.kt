6e10080ef48fd23669e8eb92ae17f6a2de0116d5
SelvaAs <136520563+SelvaAs@users.noreply.github.com>
2023-07-06 12:57:09 -0400

    Update 5-Break and Continue.kt

--- app/src/main/java/com/example/myfirstkotlinlesson/5-Break and Continue.kt
+++ app/src/main/java/com/example/myfirstkotlinlesson/5-Break and Continue.kt
@@ -13,7 +13,7 @@ fun main(){
         for (i in 1 until 20) {
 
             // 10/2=5
-            // 11/2=5.5 which is 5 in term of an int
+            // 11/2=5.5 which is 5 in term of an integer
             if (i / 2 == 5) {
               continue
             }
